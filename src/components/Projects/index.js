fd656c35cec48126cac8c3b07e80d30a3868d07b
MarketingPlus <benmarketingplus@gmail.com>
2021-07-20 11:35:09 +1000

    added another project to my code

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -33,3 +33,27 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="col-xs-12 col-sm-12 col-lg-4 mb-4">
+                    <div className="card text-center h-100">
+                        <div className="imageBox">
+                            <img src={`${process.env.PUBLIC_URL}/assets/images/ems.png`} className="card-img-top border-bottom"
+                                alt="Screenshot of Employee Management System" />
+                        </div>
+                        <div className="card-body mb-5 pl-0">
+                            <h3>Employee Management System</h3>
+                            <p className="card-text">
+                            A node.js application that prompts users with a series of options which is used to help manage employees which is store in a database
+                            </p>
+                            <p className="card-text">
+                                <small>
+                                    Features: JavaScript, NPM, SQL, Node.js
+                            </small>
+                            </p>
+                            <div className="gitLinks w-100 py-3 pt-3 pl-0 position-absolute">
+                                <a href="https://github.com/MarketingPlus/employee-management-system" target="_blank" className="btn" rel="noreferrer">
+                                    <i className="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div >
