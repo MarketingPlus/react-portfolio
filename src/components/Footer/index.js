924b5f2fbca25281442be18ced5abc0084fef805
MarketingPlus <benmarketingplus@gmail.com>
2021-07-20 11:53:48 +1000

    added my footer with all my information

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mengmei-tu/">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/MarketingPlus">
+                <i class="fab fa-github"></i>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="mailto:benmarketingplus@gmail.com">
+                <i class="fas fa-envelope"></i>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="/assets/resume.docx">
+                <i class="fas fa-file-pdf"></i>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="tel:0416149002">
+                <i class="fas fa-phone-volume"></i>
+            </a><p>Copyright © MarketingPlus | Benjamin</p>
+        </footer>
+    )
+}
+
+export default Footer
