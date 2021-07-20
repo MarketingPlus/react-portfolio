4e53e225797956b0103d9dac7253f5d38ace384f
MarketingPlus <benmarketingplus@gmail.com>
2021-07-20 11:27:28 +1000

    added some html for the navbar itself so it actually functions

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,3 +26,68 @@ const NavBar = () => {
         window.addEventListener("resize", trackScreenWidth);
         return () => window.removeEventListener("resize", trackScreenWidth);
     }, []);
+
+    return (
+        <nav className="navbar">
+            <div className="nav-wrapper">
+                <div className="logo">
+                </div>
+                <div className="list-wrapper">
+                    <i
+                        className="fas fa-bars"
+                        style={{ opacity: !open ? 1 : 0 }}
+                        onClick={() => { setOpen(open); }}
+                    >
+                    </i>
+                    <i
+                        className="fas fa-times"
+                        style={{ opacity: open ? 1 : 0 }}
+                        onClick={() => { setOpen(!open); }}
+                    >
+                    </i>
+
+                    <ul style={{ left: open ? "0" : "-100vw" }}>
+                        <li>
+                            <Link
+                                to="/"
+                                onClick={handleClose}
+                                style={{ color: location.pathname === "/" && "#afbdc9" }}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/about"
+                                onClick={handleClose}
+                                style={{ color: location.pathname === "/about" && "#afbdc9" }}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/projects"
+                                onClick={handleClose}
+                                style={{ color: location.pathname === "/projects" && "#afbdc9" }}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/contact"
+                                onClick={handleClose}
+                                style={{ color: location.pathname === "/contact" && "#afbdc9" }}
+                            >
+                                Contact
+                        </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
